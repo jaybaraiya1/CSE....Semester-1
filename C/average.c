12b07a5440515ef1aa6acd587c3ae35da4c1009c
jaybaraiya1 <140989637+jaybaraiya1@users.noreply.github.com>
2023-08-14 09:18:48 +0530

    Add files via upload

---
+++ C/average.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int a,b,c,d;
+	
+	printf("enter value of a");
+	scanf("%d",&a);
+	printf("enter value of b");
+	scanf("%d",&b);
+	printf("enter value of c");
+	scanf("%d",&c);
+	d=(a+b+c)/3;
+	printf("%d",d);
+	
+}
