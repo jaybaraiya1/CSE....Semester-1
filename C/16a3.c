216585d90707c8157da7374c61c8feb50dabd1d6
jaybaraiya1 <140989637+jaybaraiya1@users.noreply.github.com>
2023-10-26 11:06:59 +0530

    Add files via upload

---
+++ C/16a3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main(){
+	int arr[3][1],a,b;
+	for(a=0;a<3;a++){
+		for(b=0;b<1;b++)
+		{   printf("roll number\v%d",a);
+             scanf("%d",&arr[a][b]);	
+		}
+	printf("\n");
+	
+}for(a=0;a<3;a++){
+		for(b=0;b<1;b++)
+		{   
+            printf("roll number%d \vmarks%d",a,arr[a][b]);	
+		}
+	printf("\n");
+	
+
+}
+	
+}
